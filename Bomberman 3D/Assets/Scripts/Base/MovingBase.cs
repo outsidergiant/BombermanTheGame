be420248cdf77e81f4c49149b9fb11bbc61c23d8
Paul <sirdjpavline@yandex.ru>
2017-03-25 02:11:32 +0300

    Add files via upload

---
+++ Bomberman 3D/Assets/Scripts/Base/MovingBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingBase : MonoBehaviour {
+
+    protected abstract void AttemptMove(int xDir, int zDir);
+
+    protected void TurnHead(int xDir, int zDir)
+    {
+        if (xDir == -1)
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        if (xDir == 1)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        if (zDir == -1)
+            transform.eulerAngles = new Vector3(0, 90, 0);
+        if (zDir == 1)
+            transform.eulerAngles = new Vector3(0, -90, 0);
+    }
+
+    protected bool Move(int xDir, int zDir, out RaycastHit hit)
+    {
+        Vector3 start = transform.position;
+        Vector3 end = start + new Vector3(xDir, 0f, zDir);
+        Debug.Log(Physics.Linecast(start, end, out hit));
+        if (hit.transform == null)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Bomberman 3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MovingBase
+{
+
+    public float speed;
+    public Rigidbody rigidBody;
+    private int frameCounter = 0;
+    private Vector3 end;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        int horizontal = 0;
+        int vertical = 0;
+        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+        vertical = (int)(Input.GetAxisRaw("Vertical"));
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+        if (horizontal != 0 || vertical != 0)
+        {
+            AttemptMove(horizontal, vertical);
+        }
+
+        if (transform.position != end && end.x != 0 && end.y != 0 && end.z != 0)
+        {
+            frameCounter++;
+            if (frameCounter == 10)
+            {
+                frameCounter = 0;
+                end = transform.position;
+            }
+            float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+            Vector3 newPostion = Vector3.MoveTowards(rigidBody.position, end, speed * Time.deltaTime);
+
+            transform.position = newPostion;
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+        }
+    }
+
+    protected override void AttemptMove(int xDir, int zDir)
+    {
+        TurnHead(xDir, zDir);
+        RaycastHit hit;
+        if (Move(xDir, zDir, out hit))
+        {
+            end = transform.position + new Vector3(xDir, 0f, zDir);
+            //Sounds...
+        }
+
+    }
+
+    
+
+    
+}
